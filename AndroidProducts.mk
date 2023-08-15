9224799e6cf98081d3947c99eaa2177bbe5a7ac2
vherawidatama <vherawidatama@gmail.com>
2023-08-15 06:57:27 +0000

    DT Lunaa for SuperiorOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023 The superiorOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lunaa.mk
+    $(LOCAL_DIR)/superior_lunaa.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lunaa-user \
-    lineage_lunaa-userdebug \
-    lineage_lunaa-eng
+    superior_lunaa-user \
+    superior_lunaa-userdebug \
+    superior_lunaa-eng
--- superior_lunaa.mk
+++ superior_lunaa.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023 The superiorOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,10 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lunaa device
 $(call inherit-product, device/realme/lunaa/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from Realme Camera
+$(call inherit-product, vendor/realme/camera/realmecamera.mk)
 
-PRODUCT_NAME := lineage_lunaa
+# Inherit some common superior stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+
+PRODUCT_NAME := superior_lunaa
 PRODUCT_DEVICE := lunaa
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
@@ -24,6 +27,24 @@ PRODUCT_SYSTEM_NAME := RE54ABL1
 PRODUCT_SYSTEM_DEVICE := RE54ABL1
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+#ADD CONFIG
+TARGET_INCLUDE_PIXEL_CHARGER := true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+TARGET_FACEUNLOCK_SUPPORTED := true
+BUILD_WITH_GAPPS := true
+TARGET_SUPPORTS_BLUR := true
+SUPERIOR_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+#Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Maintainer
+PRODUCT_SYSTEM_PROPERTIES += \
+     ro.spos.maintainer=SMGLaboratory
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="RMX3360-user 13 TP1A.220905.001 R.e9a1c9_63b61_5d8e0 release-keys" \
